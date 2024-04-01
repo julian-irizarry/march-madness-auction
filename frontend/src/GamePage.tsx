4cc049d8a86241c66ae9a6859aea601561b1b651
Sharon Liu <sharonliu0708@gmail.com>
2024-04-01 15:28:06 -0400

    lining up

--- frontend/src/GamePage.tsx
+++ frontend/src/GamePage.tsx
@@ -80,7 +80,7 @@ function GamePage() {
                             {/* Team to bid on */}
                             <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                                 <Typography justifyContent="center">
-                                    <span style={{ fontFamily: 'threeDim2', fontSize: '60px', lineHeight: '40px' }}> {team[0]} </span>
+                                    <span style={{ fontFamily: 'threeDim2', fontSize: '60px', lineHeight: '45px' }}> {team[0]} </span>
                                     <span style={{ fontSize: '30px' }}> ({team[1]}) </span>
                                 </Typography>
                             </Grid>
