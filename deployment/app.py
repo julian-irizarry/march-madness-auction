28742d8ed3307d1501a632d10ab1ddd9a8c85de2
Julian Irizarry <julianirizarry@live.com>
2026-04-01 14:03:01 -0400

    feat: scaffold CDK project with app entry point

---
+++ deployment/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import aws_cdk as cdk
+
+from stacks import OidcStack, InfraStack
+
+app = cdk.App()
+
+env = cdk.Environment(
+    account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
+    region="us-east-1",
+)
+
+OidcStack(app, "MarchMadnessOidc", env=env)
+
+image_tag = app.node.try_get_context("image-tag") or "latest"
+InfraStack(app, "MarchMadnessInfra", env=env, image_tag=image_tag)
+
+app.synth()
---
+++ deployment/stacks/__init__.py
@@ -0,0 +1,2 @@
+from .oidc_stack import OidcStack
+from .infra_stack import InfraStack
